c8dfba3cc1f281e5c7dd8a9ac6c47379b6b6069d
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2022-02-11 16:06:35 +0530

    feat: month added

--- client/src/components/Month/Month.js
+++ client/src/components/Month/Month.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Month.css';
 
 const Month = () => {
@@ -11,17 +11,45 @@ const Month = () => {
 		'Friday',
 		'Saturday',
 	];
+	const todaysDate = new Date();
+	const [month, day, year] = [
+		todaysDate.getMonth(),
+		todaysDate.getDate(),
+		todaysDate.getFullYear(),
+	];
+	const daysInMonth = new Date(year, month + 1, 0).getDate();
+	const startOfMonthDay = new Date(year, month, 1).getDay() + 1;
+	console.log(startOfMonthDay, daysInMonth);
+
+	const getWeeksUi = () => {
+		let started = false;
+		let currentDay = 0;
+		const days = [];
+		for (let i = 0; currentDay < daysInMonth; i++) {
+			console.log('first');
+			if (i + 1 === startOfMonthDay || started) {
+				started = true;
+				currentDay++;
+				days.push(currentDay);
+			} else days.push('');
+		}
+
+		const weeksJsx = (
+			<Fragment>
+				{days.map((day) => (
+					<span className='day-name'>{day}</span>
+				))}
+			</Fragment>
+		);
+		return weeksJsx;
+	};
 	return (
 		<div className='month-calendar-container'>
 			<div className='month-calendar'>
 				{weeekDayas.map((weekday) => (
 					<span className='day-name'>{weekday}</span>
 				))}
-				{Array(31)
-					.fill('')
-					.map((el, i) => (
-						<span className='day-name pointer'>{i + 1}</span>
-					))}
+				{getWeeksUi()}
 			</div>
 		</div>
 	);
