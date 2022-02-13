fa42b8ddca10da96bc8be2851f300dc1a3ea3444
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2022-02-13 18:48:52 +0530

    feat: event form elements added

--- client/src/components/EventForm/EventForm.js
+++ client/src/components/EventForm/EventForm.js
@@ -4,20 +4,26 @@ import './EventForm.css';
 const EventForm = () => {
 	const [Event, setEvent] = useState({
 		title: '',
+		description: '',
 	});
 	const onChangeHandler = (e) => {
 		setEvent({ ...Event, [e.target.name]: e.target.value });
 	};
 	return (
 		<div className='event-form-container'>
+			<label className='input-labels'>Event Title</label>
 			<input
 				type='text'
 				name='title'
-				placeholder='Add Event Title'
 				value={Event.title}
 				onChange={onChangeHandler}
 			/>
-			<textarea />
+			<label className='input-labels'>Event Description</label>
+			<textarea
+				name='description'
+				value={Event.description}
+				onChange={onChangeHandler}
+			/>
 		</div>
 	);
 };
