dd8e0e3e72e2b5e2ea4d562162c8cca7ec5650e7
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2022-02-23 20:45:05 +0530

    feat: minicalendar added

--- client/src/components/MiniCalendar/MiniCalendar.js
+++ client/src/components/MiniCalendar/MiniCalendar.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useMonth } from '../../hooks/useMonth';
+import './MiniCalendar.css';
 
 const MiniCalendar = () => {
-	return <div>MiniCalendar</div>;
+	const { weekDays, date, months, updateDate } = useMonth();
+	const [month, day, year] = [
+		date.getMonth(),
+		date.getDate(),
+		date.getFullYear(),
+	];
+	return (
+		<div className='minicalendar-container'>
+			<div className='minicalendar-header'>
+				<div className='minicalendar-header-year'>
+					<span>{months[month] + ' ' + year}</span>
+				</div>
+				<div className='header-month-change'>
+					<i
+						className='fa-solid fa-angle-left pointer'
+						onClick={() =>
+							updateDate(date.setMonth(date.getMonth() - 1))
+						}></i>
+					<i
+						className='fa-solid fa-angle-right pointer'
+						onClick={() =>
+							updateDate(date.setMonth(date.getMonth() + 1))
+						}></i>
+				</div>
+			</div>
+			<div className='minicalendar'>
+				{weekDays.map((weekday, i) => (
+					<span className='minicalendar-week-name'>
+						{String(weekday)[0]}
+					</span>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default MiniCalendar;
